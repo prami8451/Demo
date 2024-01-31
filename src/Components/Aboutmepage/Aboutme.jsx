570f45070548aadb791d7e9ec293eafac6732366
Pramitha G <pramitha8451@mountzion.ac.in>
2024-01-31 21:58:50 +0530

    #

--- src/Components/Aboutmepage/Aboutme.jsx
+++ src/Components/Aboutmepage/Aboutme.jsx
@@ -3,10 +3,10 @@ import Image from '../../assets/aboutUs.jpg';
 const Aboutme = () => {
     return (
         <div className="secpage">
-            <div className="about">About us</div>
+            <div className="about">Aboutus</div>
             <div className="double1">
                 <div className="img">
-                  <img src={Image} className="image" alt="" />
+                    <img src={Image} className="image" alt="" />
                 </div>
                 <div className="text">
                     <p>_ARTSY.ISHU_ will work as your creative partner to styleframe your brand designs , art , & illustrations with perfect ideology.Also i am doing customized works ( Painting ,Pencil  Sketch , Illustarations etc..) that you can gift  to your friends & family.
