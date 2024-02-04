ccab42b0590f6f9b05b878d334da5abaabe4dd00
Pramitha G <pramitha8451@mountzion.ac.in>
2024-02-04 15:50:10 +0530

    #

--- src/Components/Firstpage/Firstpage.jsx
+++ src/Components/Firstpage/Firstpage.jsx
@@ -2,59 +2,35 @@ import "./Firstpage.css"
 import Logo from './menu.svg';
 const Firstpage = () => {
   return (
-    <div className="first">
+    <div className="root">
+      <div className="firstcon">
 
-      <h1 className="logo">logo</h1>
-      <ul>
 
-        <li>Testimonials</li>
-        <li>Portfolio</li>
-        <li>Services</li>
-        <li>About Us</li>
-      </ul>
-      <div className="button">
-        <button className="start">Get Started</button>
+        <div className="logo">
+          <h1>Logo</h1>
+        </div>
+        <div className="links">
+          <ul>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Testimonials</li>
+          </ul></div>
+        <div className="but">
+          <button>Get Started</button>
+        </div>
       </div>
-      <p className="trans">Transforming Visions into Visuals,<br />Unleash Your Creative Journey.</p>
-      <p className="content">Delve into our design portfolio, and you'll uncover the essence of our<br />creative vision. It's a testament to our artistic prowess, where<br /> innovation meets aesthetics, breathing life into every project.</p>
 
-
-
-
-
+      <div className="image1">
+        <img src={Logo} alt="" /></div>
+      <div className="body">
+        <div className="heading">
+          Transforming Visions into Visuals,<br /> Unleash Your Creative Journey.
+        </div>
+        <div className="content">Delve into our design portfolio, and you'll uncover the essence of our<br /> creative vision. It's a testament to our artistic prowess, where<br /> innovation meets aesthetics, breathing life into every project.</div>
+      </div>
     </div>
-
   );
-  // return (
-  //   <div className="root">
-  //     <div className="navbar">
-  //       <div className="logo-div">
-  //         <h1 className="logo">Logo</h1>
-  //       </div>
-  //       <div className="nav-contents">
-  //         <div className="links">
-  //           <ul>
-  //             <li>Home</li>
-  //             <li>AboutUs</li>
-  //             <li>Services</li>
-  //             <li>Portfolio</li>
-  //           </ul>
-  //         </div>
-  //         <div className="get-started">
-  //           <button>Get Started</button>
-  //         </div>
-  //       </div>
-  //       <div className="menu">
-  //         <img src={Logo} alt="" />
-  //       </div>
-  //     </div>
-  //     <div className="body">
-  //       <div className="title"><p className="trans">Transforming Visions into Visuals,<br />Unleash Your Creative Journey.</p></div>
-  //       <div className="desc"><p className="content">Delve into our design portfolio, and you'll uncover the essence of our<br />creative vision. It's a testament to our artistic prowess, where<br /> innovation meets aesthetics, breathing life into every project.</p></div>
-  //     </div>
-  //   </div>
-  // )
-  
 };
 
 export default Firstpage;
